65e59fede4485ec930ddc33cdb2d88b828ae5770
Ved Patel <vedpatel3395@gmail.com>
2022-02-08 22:22:22 +0530

    First Commit

---
+++ C#/CSharp/Assignment2/main.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2
+{
+    public class Run
+    {
+        public static void RunThis()
+        {
+
+        }
+    }
+     abstract class GeometricSymbols
+    {
+        abstract public decimal Perimeter { get; }
+        abstract public decimal Area();
+    }
+    
+    class Triangle : GeometricSymbols
+    {   
+        private int side1,side2,side3;
+
+        public Triangle(int side1,int side2, int side3)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+            this.side3 = side3;
+        }
+
+        public override decimal Perimeter
+        {
+            get
+            {
+                return this.side1 + this.side2 +this.side3;
+            }
+        }
+        public override decimal Area()
+        {
+            
+        }
+
+    }
+    class Quadrilateral : GeometricSymbols
+    {
+
+    }
+    class Square : Quadrilateral 
+    {
+
+    }
+    class Rectangle :Quadrilateral
+    {
+
+    }
+}
